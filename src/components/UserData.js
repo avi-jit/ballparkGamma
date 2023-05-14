c9fd6e5286c116c35abd449a9dd5ece462bbe17a
Harsh Pandey <harsh.pandey.cd.phy19@itbhu.ac.in>
2023-05-14 21:54:06 +0530

    changed UI

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
+
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
+
+
+
 import { createClient } from '@supabase/supabase-js';
 import StudyBoard from './StudyBoard';
 import createState from "../lib/create-state";
+import Grid from '@mui/material/Grid';
 const supabase = createClient('https://hpcqpvygdcpwrzoldghm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwY3FwdnlnZGNwd3J6b2xkZ2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUwMzg0NTIsImV4cCI6MTk4MDYxNDQ1Mn0.-DVUVZlZGkiylcWqO7ROJ11Y86dyHcl7ex5985WDhr8');
+
 const UserData = (props) => {
     const {user} = props;
     const [keys,setkeys] = useState(null)
@@ -92,42 +92,29 @@ const UserData = (props) => {
         )
     }
   return (
-    <List sx={{ width: '100%', maxWidth: 3600, bgcolor: 'background.paper' }}>
+    <>
         {userScores?(
-        <>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}sx={{justifyContent:'center'}}>
         {
             Object.keys(userScores['playedList']).map((key,index)=>(
                 <>
-                <ListItem alignItems="flex-start" onClick={()=>getQuestions(key)}>
-        <ListItemAvatar>
-          <Avatar alt={key} src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary={key}
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Best Score : {userScores['playedList'][key]['bestScore']}
-              </Typography>
-              
-               / Correctness : {userScores['playedList'][key]['correct']}
-               
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
+                <Grid item xs={4} sx={{justifyContent:'center'}}>
+                  <Avatar   src="/static/images/avatar/1.jpg" sx={{ width: 56, height: 56, display: 'inline-block' }} onClick={()=>getQuestions(key)} /> <h6 style={{color:'white'}}>{key}</h6>
+                 
+                  
+                </Grid>
+       
+          
+          
+        
+        
+      
                 </>
             ))
-        } </>):(<></>)}
+        } </Grid>):(<></>)}
       
       
-    </List>
+    </>
   );
 }
 export default UserData;
