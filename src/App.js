846faf4ff77d301bbead0cac89081159de135d48
Harsh Pandey <harsh.pandey.cd.phy19@itbhu.ac.in>
2023-07-27 12:46:06 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 import Game from './components/Game';
 import Header from './components/Header';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-ReactGA.initialize("UA-275459114-1")
+ReactGA.initialize("G-3JLJSPT4Y5")
+ReactGA.send({hitType: "pageview", page:"/"});
 function App() {
   return (
     <div className="App" style={{backgroundColor:"#23272c"}}>
